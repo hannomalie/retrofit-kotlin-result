0f3220fad2597966508e46db5b382b5353bf92c7
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2023-01-09 19:42:39 +0100

    make test for typed api green

--- src/test/kotlin/DeletionJobClient.kt
+++ src/test/kotlin/DeletionJobClient.kt
@@ -1,3 +1,4 @@
+import com.github.michaelbull.result.Result
 import retrofit2.Call
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -6,4 +7,8 @@ interface DeletionJobClient {
     @Headers("Content-Type: application/json")
     @POST("/deletionJob")
     fun createDeletionJob(): Call<Unit>
+
+    @Headers("Content-Type: application/json")
+    @POST("/deletionJob")
+    fun createDeletionJobTyped(): Call<Result<Unit, ApiError>>
 }
--- src/test/kotlin/DeletionJobClientTest.kt
+++ src/test/kotlin/DeletionJobClientTest.kt
@@ -1,3 +1,4 @@
+import com.github.michaelbull.result.Ok
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.Assertions
@@ -9,9 +10,9 @@ import retrofit2.converter.jackson.JacksonConverterFactory
 class DeletionJobClientTest {
 
     @Test
-    fun foo() {
+    fun `create deletion job via regular api succeeds`() {
         val server = MockWebServer()
-        server.enqueue(MockResponse().setBody("hello, world!"))
+        server.enqueue(MockResponse().setResponseCode(201))
 
         val retrofit = createRetrofit(server.url("/").toString())
 
@@ -19,6 +20,22 @@ class DeletionJobClientTest {
 
         val response = deletionJobClient.createDeletionJob().execute()
         Assertions.assertTrue(response.isSuccessful)
+
+        server.shutdown()
+    }
+
+    @Test
+    fun `create deletion job via typed api succeeds`() {
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(201))
+
+        val retrofit = createRetrofit(server.url("/").toString())
+
+        val deletionJobClient = retrofit.create(DeletionJobClient::class.java)
+
+        val response = deletionJobClient.createDeletionJobTyped().execute().body()
+        Assertions.assertInstanceOf(Ok::class.java, response)
+
         server.shutdown()
     }
 
