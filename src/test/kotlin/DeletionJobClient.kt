e9667fcf20fd22ce0a08348e6031213c6f1d0690
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2023-01-09 19:36:18 +0100

    add annotations for test client interface

--- src/test/kotlin/DeletionJobClient.kt
+++ src/test/kotlin/DeletionJobClient.kt
@@ -1,7 +1,9 @@
 import retrofit2.Call
 import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface DeletionJobClient {
     @Headers("Content-Type: application/json")
+    @POST("/deletionJob")
     fun createDeletionJob(): Call<Unit>
 }
